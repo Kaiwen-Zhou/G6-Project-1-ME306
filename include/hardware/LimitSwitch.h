a74be3aaa7681ce31a2eab094f68133bde1a0422
SONGXK129 <xson392@aucklanduni.ac.nz>
2026-07-28 00:32:17 +0000

    use LimitSwitch for switch debouncing

---
+++ include/hardware/LimitSwitch.h
@@ -0,0 +1,50 @@
+#ifndef LIMIT_SWITCH_H
+#define LIMIT_SWITCH_H
+
+#include <Arduino.h>
+
+// LimitSwitch handles reading and software debouncing of a mechanical limit switch.
+// Non-blocking software debouncing using millis() to track time since last state change.
+class LimitSwitch
+{
+public:
+
+    LimitSwitch(uint8_t pin,
+                uint8_t mode = INPUT_PULLUP,
+                unsigned long debounceTime = 20);
+                // pin - Arduino digital input pin
+                // mode - INOUT or INPUT_PULLUP
+                // debounceTime - debouncing interval in milliseconds
+
+    // Initialise the switch hardware.
+    void begin();
+
+    //Updates the switch state.
+    //Reads the raw input signal, performs software debouncing, and updates the stable state.
+    void update();
+
+    //Returns true if the switch is pressed.
+    //LOW means pressed.
+    bool isPressed() const;
+
+    //Returns true if the switch is released.
+    bool isReleased() const;
+
+    //Returns the stable raw digital state. (LOW  = pressed; HIGH = released)
+    bool getState() const;
+
+private:
+    uint8_t pinNumber; // Arduino pin connected to the switch
+
+    uint8_t inputMode; // INPUT or INPUT_PULLUP
+
+    unsigned long debounceDelay; // Debounce time delay in ms
+
+    bool stableState; // Debounced stable state
+
+    bool lastReading; // Previous raw reading
+
+    unsigned long lastDebounceTime; // Time when the last state change occurred
+};
+
+#endif
